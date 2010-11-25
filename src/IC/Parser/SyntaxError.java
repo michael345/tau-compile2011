c4610c357e37a6da35bccddd1e5dffd26cd818d6
YosisoY <YosisoY@e0cc6219-3778-003b-5256-cc707d9ed48d>
2010-11-25 15:43:09 +0000

    Some more basic work for PA2,

--- src/IC/Parser/SyntaxError.java
+++ src/IC/Parser/SyntaxError.java
@@ -1,4 +1,21 @@
 package IC.Parser;
 
 public class SyntaxError extends Exception {
+
+    private static final long serialVersionUID = 1L;
+    private int line;
+    private String message;
+    private Token token; // might be needed
+    private int errorCode; // for switch case maybe later
+    
+    SyntaxError(int line, Token token, String message) { 
+        this.line = line;
+        this.message = message;
+        this.token = token;
+    }
+    
+    void printErrorMsg() { 
+        System.out.println("Line " + line + ": Token " + token.getValue() + " - " + message);
+    }
+    
 }
