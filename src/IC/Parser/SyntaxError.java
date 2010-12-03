8591edeed119a0f2fd7f6126ceaf814c07a4a3bf
YosisoY <YosisoY@e0cc6219-3778-003b-5256-cc707d9ed48d>
2010-12-03 16:44:02 +0000

    Fixed the else problem

--- src/IC/Parser/SyntaxError.java
+++ src/IC/Parser/SyntaxError.java
@@ -3,19 +3,17 @@ package IC.Parser;
 public class SyntaxError extends Exception {
 
     private static final long serialVersionUID = 1L;
-    private int line;
     private String message;
     private Token token; // might be needed
     private int errorCode; // for switch case maybe later
     
-    SyntaxError(int line, Token token, String message) { 
-        this.line = line;
+    SyntaxError(Token token, String message) { 
         this.message = message;
         this.token = token;
     }
     
     void printErrorMsg() { 
-        System.out.println("Line " + line + ": Token " + token.getValue() + " - " + message);
+        System.out.println("Line " + token.getLine() + ": Token " + token.getValue() + " - " + message);
     }
     
 }
