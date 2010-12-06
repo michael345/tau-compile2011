5fe10f7d5a2be22fbfc913daadcabf31a64c5136
oded100oren <oded100oren@yahoo.com>
2010-12-06 19:54:10 +0000

    CHANGED syntaxError.java A BIT AS WELL, BUT JUST ESTHETICS, NOW ITS UGLIER

--- src/IC/Parser/SyntaxError.java
+++ src/IC/Parser/SyntaxError.java
@@ -4,16 +4,21 @@ public class SyntaxError extends Exception {
 
     private static final long serialVersionUID = 1L;
     private String message;
-    private Token token; // might be needed
+    private Token token;
     private int errorCode; // for switch case maybe later
     
-    SyntaxError(Token token, String message) { 
+
+    SyntaxError(Token token) { 
+
         this.message = message;
         this.token = token;
     }
+   
     
     void printErrorMsg() { 
-        System.out.println("Line " + token.getLine() + ": Token " + token.getValue() + " - " + message);
+
+        System.out.println("Line " + token.getLine() + ", Problem on Token " + ": " + Utils.tokenIDToString(token.getId()));
+
     }
-    
 }
+
