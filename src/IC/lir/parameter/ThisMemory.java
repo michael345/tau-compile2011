537ad475aaa26821d69cf0465fcb9e419c763cc0
YosisoY <YosisoY@e0cc6219-3778-003b-5256-cc707d9ed48d>
2011-01-26 15:44:23 +0000

    Our latest work. EPIC

--- src/IC/lir/parameter/ThisMemory.java
+++ src/IC/lir/parameter/ThisMemory.java
@@ -3,10 +3,14 @@ package IC.lir.parameter;
 public class ThisMemory extends LIRMemory {
 
     public ThisMemory() {
-        super("this");
+        super(null);
+        
+        
     }
     
-    
+    public String toString() {
+        return "this";
+    } 
 
     
 }
